0d10b055927bfab86dbf9f0e9ed87c9114f6237d
Arkhin <nabillouh62110@gmail.com>
2022-05-20 10:55:42 +0200

    fonctionnement du CLap avec souris

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 //Avec le clavier
 
-document.body.addEventListener('keypress', function(event){
+document.body.addEventListener('keydown', function(event){
     if (event.key =="q") {
         console.log('ok, jusque la');
         document.body.style.backgroundColor = "#568259"
@@ -37,4 +37,40 @@ document.body.addEventListener('keyup', function(event){
     }
 
 });
+
+
+document.getElementById('q').addEventListener('mousedown', function(event){
+    
+        console.log('ok, jusque la');
+        document.body.style.backgroundColor = "#568259"
+        document.getElementById('container').style.backgroundColor = "#568259"
+        document.getElementById('batterie').style.backgroundColor = "#568259"
+        document.getElementById('batterie').style.color = "yellow"
+        document.getElementById('batterie').style.border = "5px yellow solid"
+        document.getElementById('batterie').style.boxShadow = "0px 0px 20px 10px rgba(188,192,18,0.7)"
+        document.getElementById('q').style.color = "yellow"
+        document.getElementById('q').style.border = "3px yellow solid"
+        document.getElementById('q').style.boxShadow = "0px 0px 20px 10px rgba(188,192,18,0.7)"
+        document.getElementById('clap').play();
+
+});
+
+
+
+document.getElementById('q').addEventListener('mouseup', function(event){
+
+        console.log('ok, jusque la');
+        document.body.style.backgroundColor = "black"
+        document.getElementById('container').style.backgroundColor = "black"
+        document.getElementById('batterie').style.backgroundColor = "black"
+        document.getElementById('batterie').style.color = "white"
+        document.getElementById('batterie').style.border = "5px black solid"
+        document.getElementById('q').style.color = "white"
+        document.getElementById('q').style.border = "3px white solid"
+        document.getElementById('batterie').style.boxShadow = "0px 0px 0px 0px rgba(188,192,18,0.7)"
+        document.getElementById('q').style.boxShadow = "0px 0px 0px 0px rgba(188,192,18,0.7)"
+        document.getElementById('clap').currentTime = 0;
+
+});
+
 
